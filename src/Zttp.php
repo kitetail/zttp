96eb17aa20c12852f8872bd32c839d35e08f1d4f
Brandon Surowiec <BrandonSurowiec@users.noreply.github.com>
2017-05-26 11:47:55 -0400

    Check whether tap() has been defined already

--- src/Zttp.php
+++ src/Zttp.php
@@ -178,7 +178,9 @@ class ZttpResponse
     }
 }
 
-function tap($value, $callback) {
-    $callback($value);
-    return $value;
+if (! function_exists('tap')) {
+    function tap($value, $callback) {
+        $callback($value);
+        return $value;
+    }
 }
