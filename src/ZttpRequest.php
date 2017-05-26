105c320cfeda99e16cb7d95cf98bf74a065c48e1
ARCANEDEV <arcanedev.maroc@gmail.com>
2017-05-26 20:06:31 +0000

    Adding a `helpers.php` file with the `tap()` (namespaced global function)

--- src/ZttpRequest.php
+++ src/ZttpRequest.php
@@ -208,17 +208,3 @@ class ZttpRequest
         });
     }
 }
-
-/**
- * Call the given Closure with the given value then return the value.
- *
- * @param  mixed          $value
- * @param  callable|null  $callback
- *
- * @return mixed
- */
-function tap($value, $callback = null)
-{
-    $callback($value);
-    return $value;
-}
