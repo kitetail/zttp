078f96bb8975ba2138c9345fa3ea929f7527d32f
Brian Faust <faustbrian@users.noreply.github.com>
2017-07-13 08:44:11 +0300

    Add asMultipart method

--- src/Zttp.php
+++ src/Zttp.php
@@ -54,6 +54,11 @@ class PendingZttpRequest
         return $this->bodyFormat('form_params')->contentType('application/x-www-form-urlencoded');
     }
 
+    public function asMultipart()
+    {
+        return $this->bodyFormat('multipart')->contentType('multipart/form-data');
+    }
+
     function bodyFormat($format)
     {
         return tap($this, function ($request) use ($format) {
